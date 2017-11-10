569d4fdad4eb4ed9a7dae05eb0a702436c912f63
Groskilled <adam.wybierala@gmail.com>
2017-11-10 15:42:09 +0100

    keeping model and main up to date

--- main.py
+++ main.py
@@ -7,56 +7,64 @@ import glob
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 import math
+from scipy.ndimage.measurements import label
+from moviepy.editor import VideoFileClip
 
-def get_img_parts(img):
+def get_img_parts(img, sizex):
     imgs = []
-    for i in range(0,240, 120):
-        for j in range(0,1278, 213):
-            tmp = cv2.resize(img[i:i+120,j:j+213], (64,64))/255
-            imgs.append(tmp)
-            tmp = cv2.resize(img[i:i+120,j+107:j+320], (64,64))/255
-            imgs.append(tmp)
-            tmp = cv2.resize(img[i+60:i+180,j+107:j+320], (64,64))/255
-            imgs.append(tmp)
-            tmp = cv2.resize(img[i+60:i+180,j:j+213], (64,64))/255
-            imgs.append(tmp)
-            #plt.imshow(tmp)
-            #plt.show()
+    x = img.shape[1]
+    for i in range(0,x, int(sizex * 0.3)):
+        if i + sizex > x + sizex:
+            break
+        tmp = img[:,i:i+sizex]
+        imgs.append(cv2.resize(tmp,(64,64))/255)
     return imgs
 
+def heat_map(heatmap, boxes, x, box_size, y_top, y_bot):
+    for box in boxes:
+        heatmap[y_top:y_bot, box * x: box * x + box_size] += 1
+    return heatmap
+
 def train_model():
-    batch_size = 20
-    model = Model()
+    batch_size = 32
+    model = Model(pretrained=True)
     test_datagen = ImageDataGenerator(rescale=1./255)
-    train_datagen = ImageDataGenerator(rescale=1./255,shear_range=0.2,zoom_range=0.2,horizontal_flip=True)
+    train_datagen = ImageDataGenerator(rescale=1./255,shear_range=0.5,zoom_range=0.2,horizontal_flip=True, rotation_range=0.5,width_shift_range=0.3,height_shift_range=0.3)
     train_generator = train_datagen.flow_from_directory('train_images',target_size=(64, 64),batch_size=batch_size,class_mode='binary')
     validation_generator = test_datagen.flow_from_directory('val_images',target_size=(64, 64),batch_size=batch_size,class_mode='binary')
-    model.fit_generator(train_generator,epochs=15, steps_per_epoch=9553 // batch_size, validation_data=validation_generator, validation_steps=6367 // batch_size)
-    model.save_weights('weights_1.h5')
+    model.fit_generator(train_generator,epochs=25, steps_per_epoch=9553 // batch_size, validation_data=validation_generator, validation_steps=6367 // batch_size)
+    model.save_weights('weights_fcn.h5')
+
+def draw_labeled_bboxes(img, labels):
+    for car_number in range(1, labels[1]+1):
+        nonzero = (labels[0] == car_number).nonzero()
+        nonzeroy = np.array(nonzero[0])
+        nonzerox = np.array(nonzero[1])
+        bbox = ((np.min(nonzerox), np.min(nonzeroy)), (np.max(nonzerox), np.max(nonzeroy)))
+        cv2.rectangle(img, bbox[0], bbox[1], (0,0,255), 6)
+    return img
 
 #train_model()
 #sys.exit()
 model = Model()
-model.load_weights('weights_1.h5')
+model.load_weights('weights_fcn.h5')
 images = glob.glob('test_images/*.jpg')
 pos_lst = []
 for idx,fname in enumerate(images):
     img = mpimg.imread(fname)
-    img = img[400:640,1:1279]
-    #cv2.line(img, (0,120),(1278,120),(255,0,0),2)
-    #cv2.line(img, (213,0),(213,240),(255,0,0),2)
-    #cv2.line(img, (426,0),(426,240),(255,0,0),2)
-    #cv2.line(img, (639,0),(639,240),(255,0,0),2)
-    #cv2.line(img, (852,0),(852,240),(255,0,0),2)
-    #cv2.line(img, (1065,0),(1065,240),(255,0,0),2)
-    #cv2.line(img, (1278,0),(1278,240),(255,0,0),2)
-    #print(img.shape)
-    #for i in range(0,240, 80):
-    #    for j in range(0,1280, 80):
-    #        tmp = img[i:i+80,j:j+80]
-    #        imgs.append(cv2.resize(tmp,(64,64))/255)
-    imgs = np.array(get_img_parts(img))
-    ret = np.where(model.predict(imgs) > 0.95)[0]
-    for p in ret:
-        plt.imshow(imgs[p])
-        plt.show()
+    tmp = img[400:656]
+
+    img_size = 32 * 2
+    
+    imgs1 = np.array(get_img_parts(tmp[:img_size], img_size))
+    cars1 = np.where(model.predict(imgs1) > 0.5)[0]
+    
+    heat = np.zeros_like(tmp[:,:,0]).astype(np.float)
+    heat = heat_map(heat, cars1, int(img_size*0.3), img_size, 0, img_size)
+
+    heat = np.clip(heat,0,255)
+    labels = label(heat)
+
+    draw_img = draw_labeled_bboxes(np.copy(tmp), labels)
+    plt.imshow(draw_img)
+    plt.show()
--- model.py
+++ model.py
@@ -9,33 +9,46 @@ from keras.applications.imagenet_utils import _obtain_input_shape
 import keras.backend as K
 import tensorflow as tf
 from keras.models import Sequential
-from keras.layers.core import Flatten, Dense, Dropout, Lambda
-from keras.layers.convolutional import Conv2D
+from keras import optimizers
+from keras.layers.core import Flatten, Dense, Dropout
+from keras.layers.convolutional import Conv2D, UpSampling2D
 
-def Model():
-
+def Model(pretrained=False):
     model = Sequential()
-    model.add(Conv2D(64, (7,7), strides=(2,2), activation='relu', input_shape=(64,64,3)))
-
-    model.add(Conv2D(192, (3, 3), strides=(2,2), activation='relu'))
-
-    model.add(Conv2D(128, (1, 1), activation='relu', padding='same'))
-    model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
-
-    model.add(Conv2D(256, (1, 1), activation='relu', padding='same'))
-    model.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
-    model.add(Conv2D(512, (3, 3), strides=(3,3), activation='relu', padding='same'))
+    model.add(Conv2D(64, (3, 3), activation='relu', padding='same', input_shape=(64,64,3)))
+    model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+    model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2))) 
     
-    model.add(Conv2D(128, (1, 1), activation='relu', padding='same'))
     model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
-    model.add(Conv2D(512, (3, 3), strides=(3,3), activation='relu', padding='same'))
+    model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+    
+    model.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+    model.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+    if pretrained == True:
+        model.load_weights('pretrained.h5')
+        for layer in model.layers:
+            layer.trainable=False
 
     model.add(Flatten())
-    model.add(Dense(4096, activation='relu'))
-    #model.add(Dense(512, activation='relu'))
-    model.add(Dropout(0.4))
-    model.add(Dense(1, activation='sigmoid'))
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(0.5))
 
-    model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
+    model.add(Dense(1,activation='sigmoid'))
+    optimizer=optimizers.Adam(lr=1e-2)
+    model.compile(loss='binary_crossentropy',optimizer=optimizer,metrics=['accuracy'])
     return model
-
